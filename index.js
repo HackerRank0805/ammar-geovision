9e55749a5a623a1903aa651cf3a0ce8b580d8419
HackerRank0805 <100253838+HackerRank0805@users.noreply.github.com>
2024-01-11 04:34:59 -0700

    curve path & smooth transition

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const horizontalSections = gsap.utils.toArray('section.horizontal')
-const animationWrap = document.querySelector('.animation-wrap').offsetWidth;
-
-
+const horizontalSections = gsap.utils.toArray("section.horizontal");
+const animationWrap = document.querySelector(".animation-wrap").offsetWidth;
 
 horizontalSections.forEach(function (sec, i) {
-
-  var thisPinWrap = sec.querySelector('.pin-wrap');
-  var thisAnimWrap = thisPinWrap.querySelector('.animation-wrap');
+  var thisPinWrap = sec.querySelector(".pin-wrap");
+  var thisAnimWrap = thisPinWrap.querySelector(".animation-wrap");
   var getToValue = () => -(thisAnimWrap.scrollWidth - window.innerWidth);
 
   // gsap.fromTo(thisAnimWrap, { x: 0 }, {
@@ -23,21 +20,159 @@ horizontalSections.forEach(function (sec, i) {
   //     //markers: true,
   //   }
   // });
-
   var endValue = () => "+=" + (thisAnimWrap.scrollWidth - window.innerWidth);
-
-  let scrollTween = gsap.fromTo(thisAnimWrap, { x: 0 }, {
-    x: getToValue(),
-    ease: "none",
-    scrollTrigger: {
-      trigger: sec,
-      start: "top top",
-      end: endValue, // use a calculated value
-      pin: thisPinWrap,
-      invalidateOnRefresh: true,
-      scrub: true,
+  var endValue2 = () => "+=" + (thisAnimWrap.scrollWidth - window.innerWidth);
+  console.log(thisAnimWrap.scrollWidth - window.innerWidth - 100);
+  let scrollTween = gsap.fromTo(
+    thisAnimWrap,
+    { x: 0 },
+    {
+      x: getToValue(),
+      ease: "none",
+      scrollTrigger: {
+        trigger: sec,
+        start: "top 0",
+        end: endValue, // use a calculated value
+        // pin: thisPinWrap,
+        invalidateOnRefresh: true,
+        scrub: true,
+        // markers: true
+      },
     }
-  });
+  );
+
+
+
+  gsap.timeline({
+    scrollTrigger:
+    {
+      trigger: ".anim-height",
+      start: "0% 0%",
+      end: "100% 100%", // use a calculated value
+      scrub: true,
+      pin: thisPinWrap,
+      // markers: true
+    },
+  })
+
+  gsap.timeline({
+    scrollTrigger:
+    {
+      trigger: ".transition-trigger-1",
+      start: "0% 80%",
+      end: "100% 20%", // use a calculated value
+      scrub: true,
+      // markers: true
+    },
+  })
+    .to(".brown-overlay", {
+      opacity: 1
+    })
+
+  gsap.timeline({
+    scrollTrigger:
+    {
+      trigger: ".transition-trigger-1",
+      start: "0% -30%",
+      end: "100% -80%", // use a calculated value
+      scrub: true,
+      // markers: true
+    },
+  })
+    .to(".brown-overlay", {
+      opacity: 0
+    })
+
+
+
+  // Section 2
+
+  gsap.timeline({
+    scrollTrigger:
+    {
+      trigger: ".transition-trigger-2",
+      start: "0% 200%",
+      end: "100% 80%", // use a calculated value
+      scrub: true,
+      // markers: true
+    },
+  })
+    .to(".brown-overlay", {
+      opacity: 1
+    }, 0)
+    .to(".drill_s", {
+      scale: 10,
+      transformOrigin: "center bottom"
+    }, 0)
+
+  gsap.timeline({
+    scrollTrigger:
+    {
+      trigger: ".transition-trigger-2",
+      start: "0% 20%",
+      end: "100% -80%", // use a calculated value
+      scrub: true,
+      // markers: true
+    },
+  })
+    .to(".brown-overlay", {
+      opacity: 0
+    }, 0)
+
+
+
+  // Section 3
+
+  gsap.timeline({
+    scrollTrigger:
+    {
+      trigger: ".transition-trigger-3",
+      start: "0% 100%",
+      end: "100% 20%", // use a calculated value
+      scrub: true,
+      // markers: true
+    },
+  })
+    .to(".brown-overlay", {
+      opacity: 1
+    }, 0)
+    .to(".drilling .drill_l", {
+      scale: 0.1,
+      transformOrigin: "center bottom"
+    }, 0)
+
+  gsap.timeline({
+    scrollTrigger:
+    {
+      trigger: ".transition-trigger-3",
+      start: "0% 20%",
+      end: "100% -80%", // use a calculated value
+      scrub: true,
+      // markers: true
+    },
+  })
+    .to(".brown-overlay", {
+      opacity: 0
+    }, 0)
+
+
+  // let scrollTween2 = gsap.fromTo(
+  //   thisAnimWrap,
+  //   { x: 0 },
+  //   {
+  //     x: getToValue(),
+  //     ease: "none",
+  //     scrollTrigger: {
+  //       trigger: sec,
+  //       start: "top 0",
+  //       end: endValue, // use a calculated value
+  //       pin: thisPinWrap,
+  //       invalidateOnRefresh: true,
+  //       scrub: true,
+  //       markers: true
+  //     },
+  //   }
+  // );
 
   // gsap.fromTo(".truck", { x: 500 }, {
   //   x: 1000,
@@ -65,31 +200,39 @@ horizontalSections.forEach(function (sec, i) {
   //   },
   // })
 
-  gsap.fromTo(".truck", { x: 0 }, {
-    x: 2000,
-    ease: "none",
-    scrollTrigger: {
-      containerAnimation: scrollTween,
-      trigger: '#section_2',
-      start: "left left",
-      end: "+=14000", // use the same calculated value
-      scrub: true,
-      markers: true,
-    },
-  });
+  gsap.fromTo(
+    ".truck",
+    { x: 0 },
+    {
+      x: 2100,
+      ease: "none",
+      scrollTrigger: {
+        containerAnimation: scrollTween,
+        trigger: "#section_2",
+        start: "left left",
+        end: "+=24000", // use the same calculated value
+        scrub: true,
+        // markers: true,
+      },
+    }
+  );
 
-  gsap.fromTo(".truckwheel", { x: 0 }, {
-    x: 2000,
-    ease: "none",
-    scrollTrigger: {
-      containerAnimation: scrollTween,
-      trigger: '#section_2',
-      start: "left left",
-      end: "+=14000",
-      scrub: true,
-      markers: true,
-    },
-  })
+  gsap.fromTo(
+    ".truckwheel",
+    { x: 0 },
+    {
+      x: 2100,
+      ease: "none",
+      scrollTrigger: {
+        containerAnimation: scrollTween,
+        trigger: "#section_2",
+        start: "left left",
+        end: "+=24000",
+        scrub: true,
+        // markers: true,
+      },
+    }
+  );
 
   // gsap.to("#man", {
   //   x: "+=280",
@@ -109,17 +252,17 @@ horizontalSections.forEach(function (sec, i) {
   const man_tl = gsap.timeline({
     scrollTrigger: {
       containerAnimation: scrollTween,
-      trigger: "#section_10",
-      start: "left center",
-      end: "+=800",
+      trigger: "#section_12",
+      start: "top center",
+      end: "+=1500",
       scrub: true,
-      markers: true,
-    }
+      // markers: true,
+    },
   });
 
   man_tl
-    .to('#man', { x: "+80", ease: "linear", duration: 2, bottom: "18%" }, 0)
-    .to('#man', { opacity: 0 }, 3);
+    .to("#man", { x: "+325", ease: "linear", duration: 1.5, bottom: "16%" }, 0)
+    .to("#man", { opacity: 0 }, 3);
 });
 
 // gsap.fromTo(".truck", { x: 0 }, {
@@ -133,23 +276,31 @@ horizontalSections.forEach(function (sec, i) {
 //   },
 // })
 
-gsap.fromTo(".bg_mountain", { x: 0 }, {
-  x: -1000,
-  ease: "none",
-  scrollTrigger: {
-    trigger: '.horizontal',
-    scrub: true,
-  },
-})
+gsap.fromTo(
+  ".bg_mountain",
+  { x: 0 },
+  {
+    x: -1000,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".horizontal",
+      scrub: true,
+    },
+  }
+);
 
-gsap.fromTo(".cloud", { x: 0 }, {
-  x: -2000,
-  ease: "none",
-  scrollTrigger: {
-    trigger: '.horizontal',
-    scrub: true,
-  },
-})
+gsap.fromTo(
+  ".cloud",
+  { x: 0 },
+  {
+    x: -2000,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".horizontal",
+      scrub: true,
+    },
+  }
+);
 
 // gsap.fromTo(".mountain", { x: 0 }, {
 //   x: -3000,
@@ -159,8 +310,6 @@ gsap.fromTo(".cloud", { x: 0 }, {
 //     scrub: true,
 //   },
 // })
-
-
 
 // Truck Wheel Animation
 
@@ -180,7 +329,7 @@ dev.interactions = {
           trigger: "html",
           scrub: 0.2,
           start: "top top",
-          end: "+=20000",
+          end: "+=24000",
         },
       })
       .to($button_left, {
@@ -201,7 +350,7 @@ dev.interactions = {
           trigger: "html",
           scrub: 0.2,
           start: "top top",
-          end: "+=20000",
+          end: "+=24000",
         },
       })
       .to($button_right, {
@@ -222,11 +371,33 @@ let image_s = drill_s.querySelector("img");
 let drill_l = document.querySelector(".drill_l");
 let image_l = drill_l.querySelector("img");
 
-
 // First Animation
 
-gsap.fromTo(drill_s, { y: 0, delay: 3 }, {
-  y: 900,
+// gsap.fromTo(
+//   drill_s,
+//   { y: 0, delay: 3 },
+//   {
+//     y: 900,
+//     immediateRender: false,
+//     scrollTrigger: {
+//       trigger: drill_s,
+//       start: "bottom 0%",
+//       end: "+=10000",
+//       scrub: true,
+//       pin: true,
+
+//       onUpdate: (self) => {
+//         let progress = self.progress.toFixed(2);
+//         let imageIndex = Math.floor(progress * 50) % 5;
+//         let imageUrl = `./assets/drill/drill_${imageIndex + 1}.svg`;
+//         image_s.setAttribute("src", imageUrl);
+//       },
+//     },
+//   }
+// );
+
+gsap.to(drill_s, {
+  y: 700,
   immediateRender: false,
   scrollTrigger: {
     trigger: drill_s,
@@ -234,97 +405,134 @@ gsap.fromTo(drill_s, { y: 0, delay: 3 }, {
     end: "+=10000",
     scrub: true,
     pin: true,
-
+    // markers: true,
     onUpdate: (self) => {
       let progress = self.progress.toFixed(2);
       let imageIndex = Math.floor(progress * 50) % 5;
       let imageUrl = `./assets/drill/drill_${imageIndex + 1}.svg`;
-      image_s.setAttribute('src', imageUrl);
+      image_s.setAttribute("src", imageUrl);
     },
   },
 });
 
-gsap.fromTo(drill_l, { y: 0 }, {
-  y: 900,
-  immediateRender: false,
-  scrollTrigger: {
-    trigger: drill_l,
-    start: "bottom 0%",
-    end: "+=10000",
-    scrub: true,
-    pin: true,
+gsap.fromTo(
+  drill_l,
+  { y: 0 },
+  {
+    y: 900,
+    immediateRender: false,
+    scrollTrigger: {
+      trigger: drill_l,
+      start: "bottom 0%",
+      end: "+=10000",
+      scrub: true,
+      pin: true,
 
-    onUpdate: (self) => {
-      let progress = self.progress.toFixed(2);
-      let imageIndex = Math.floor(progress * 60) % 6;
-      let imageUrl = `./assets/drill_0${imageIndex + 1}.svg`;
-      image_l.setAttribute('src', imageUrl);
+      onUpdate: (self) => {
+        let progress = self.progress.toFixed(2);
+        let imageIndex = Math.floor(progress * 60) % 6;
+        let imageUrl = `./assets/drill_0${imageIndex + 1}.svg`;
+        image_l.setAttribute("src", imageUrl);
+      },
     },
-  },
-});
+  }
+);
 
 // Motion Path
 
 var ww = window.innerWidth,
   wh = window.innerHeight,
-  speed = 20,
+  speed = 10,
   scrollDist = wh * speed,
   scrollEnd = wh * (speed - 1),
-  map = document.getElementById('map'),
+  map = document.getElementById("map"),
   mapWidth = map.getBoundingClientRect().width,
   mapHeight = map.getBoundingClientRect().height;
 
-gsap.set('#scrollDist', { width: '100%', height: scrollDist })
-gsap.set('#container', { position: 'fixed', width: mapWidth, height: mapHeight, transformOrigin: '0 0', left: "50%", top: "-100px", autoAlpha: 1 })
-gsap.set('#point', { scale: 1, opacity: 1 });
-gsap.set('#path', { scale: 1, opacity: 1 });
+gsap.set("#scrollDist", { width: "100%", height: scrollDist });
+gsap.set("#container", {
+  position: "fixed",
+  width: mapWidth,
+  height: mapHeight,
+  transformOrigin: "0 0",
+  left: "50%",
+  top: "-100px",
+  autoAlpha: 1,
+});
+gsap.set("#point", { scale: 1, opacity: 1 });
+gsap.set("#path", { scale: 1, opacity: 1 });
 
-gsap.timeline({
-  defaults: { duration: 1, ease: 'none' },
-  scrollTrigger: {
-    trigger: '#scrollDist',
-    start: 'top center',
-    end: '+=' + scrollEnd,
-    scrub: 0.3,
-    onUpdate: ({ progress }) => console.log(progress) //  info for position
-  }
-})
-  .to('#point', {
-    opacity: 1,
-    motionPath: {
-      path: "#path",
-      align: "#path",
-      alignOrigin: [0.5, 0.5],
-      autoRotate: true,
+gsap
+  .timeline({
+    defaults: { duration: 1, ease: "none" },
+    scrollTrigger: {
+      trigger: "#scrollDist",
+      start: "top center",
+      end: "+=" + scrollEnd,
+      scrub: 0.6,
+      onUpdate: ({ progress }) => console.log(progress),
     },
-  }, 0)
-  .to('#path', {
-    opacity: 1,
-  }, 0)
-  .to('#container', {
-    opacity: 1,
-    left: "50%",
-    top: "80%"
-  }, 0)
+  })
+  .to(
+    "#point",
+    {
+      opacity: 1,
+      motionPath: {
+        path: "#path",
+        align: "#path",
+        alignOrigin: [0.7, 0.5],
+        autoRotate: true,
+      },
+    },
+    0
+  )
+  .to(
+    "#path",
+    {
+      opacity: 1,
+    },
+    0
+  )
+  .to(
+    "#container",
+    {
+      opacity: 1,
+      left: "50%",
+      top: "80%",
+    },
+    0
+  )
 
-  .from('#path', { drawSVG: '0 0' }, 0.006)
-  .set('#point', { scale: 1 }, 1)
+  .to(
+    "#container",
+    {
+      left: "90%",
+      top: "80%",
+    },
+    1
+  )
+
+  .from("#path", { drawSVG: "0 0" }, 0.006)
+  .set("#point", { scale: 1 }, 1);
 
 let povDelay = 1,
   pos = { x: -mapWidth / 2, y: -mapHeight / 2 },
-  xSet = gsap.quickSetter('#container', "x", "px"),
-  ySet = gsap.quickSetter('#container', "y", "px");
+  xSet = gsap.quickSetter("#container", "x", "px"),
+  ySet = gsap.quickSetter("#container", "y", "px");
 
 gsap.ticker.add(() => {
-  pos.x += (-gsap.getProperty('#point', 'x') - pos.x) * povDelay;
-  pos.y += (-gsap.getProperty('#point', 'y') - pos.y) * povDelay;
+  pos.x += (-gsap.getProperty("#point", "x") - pos.x) * povDelay;
+  pos.y += (-gsap.getProperty("#point", "y") - pos.y) * povDelay;
   xSet(pos.x);
   ySet(pos.y);
 });
 
 window.onresize = () => {
-  gsap.set('#container', { left: window.innerWidth / 2, top: window.innerHeight / 2 });
-}
+  gsap.set("#container", {
+    left: window.innerWidth / 2,
+    top: window.innerHeight / 2,
+  });
+};
 
 // Blog Fade in and fade out
 
@@ -334,12 +542,13 @@ const tl = gsap.timeline({
     start: "center center",
     end: "+=300 center",
     scrub: "+300",
-    markers: true,
+    // markers: true,
     toggleActions: "play reverse play reverse",
-  }
+  },
 });
 
-tl
-  .to('.blog', { opacity: 1, duration: 0.5 })
-  .to('.blog', { opacity: 0, duration: 0.5 }, 0.5)
-  ;
+tl.to(".blog", { opacity: 1, duration: 0.5 }).to(
+  ".blog",
+  { opacity: 0, duration: 0.5 },
+  0.5
+);
